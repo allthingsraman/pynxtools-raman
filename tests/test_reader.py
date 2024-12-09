5860fb5de8bf6c7e0ce0b8a4ea9d7776da020601
Ron Hildebrandt <RonHildebrandt@uni-leipzig.de>
2024-12-09 12:59:54 +0100

    initial_commit

--- tests/test_reader.py
+++ tests/test_reader.py
@@ -2,6 +2,7 @@
 Basic example based test for the stm reader
 """
 
+import pytest
 import os
 from glob import glob
 from pathlib import Path
@@ -9,12 +10,19 @@ from pathlib import Path
 from pynxtools.testing.nexus_conversion import ReaderTest
 
 
-def test_nexus_conversion(caplog, tmp_path):
+@pytest.mark.parametrize(
+    "data_dir, caplog_level",
+    [
+        ("rod", "WARNING"),
+        ("witec", "WARNING"),
+    ],
+)
+def test_nexus_conversion(data_dir, caplog_level, tmp_path, caplog):
     """
     Tests the conversion into nexus.
     """
     caplog.clear()
-    dir_path = Path(__file__).parent / "data"
+    dir_path = Path(__file__).parent / f"data/{data_dir}"
     test = ReaderTest(
         nxdl="NXraman",
         reader_name="raman",
@@ -22,17 +30,5 @@ def test_nexus_conversion(caplog, tmp_path):
         tmp_path=tmp_path,
         caplog=caplog,
     )
-    test.convert_to_nexus(caplog_level="WARNING", ignore_undocumented=True)
-    test.check_reproducibility_of_nexus()
-
-    caplog.clear()
-    dir_path_multi = Path(__file__).parent / "data_multi"
-    test = ReaderTest(
-        nxdl="NXraman",
-        reader_name="raman_multi",
-        files_or_dir=glob(os.path.join(dir_path_multi, "*")),
-        tmp_path=tmp_path,
-        caplog=caplog,
-    )
-    test.convert_to_nexus(caplog_level="WARNING", ignore_undocumented=True)
+    test.convert_to_nexus(caplog_level=caplog_level, ignore_undocumented=False)
     test.check_reproducibility_of_nexus()
